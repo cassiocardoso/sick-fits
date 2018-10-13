ac3744f9778e156b11196050b19e795b461fae32
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-10-13 17:40:19 -0300

    🎉 Add Item type to backend
    - Implemented the Item type on GrpahQL
    - Added  Mutation for createItem
    - Added Query for items

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+// forwardTo allows to query Prisma directly, it's useful for simple operations
+// where no logic or authentication is needed
+const Query = {
+  items: forwardTo("db")
+};
 
 module.exports = Query;

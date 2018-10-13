0f40e9413db57287d1b99e9f807d18b9c901f247
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-10-13 18:12:42 -0300

    🎉 Add frontend connection to Apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,24 +1,49 @@
 import App, { Container } from "next/app";
 import PropTypes from "prop-types";
+import { ApolloProvider } from "react-apollo";
 
+import withData from "../lib/withData";
 import Page from "../components/Page";
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    // SSR: this is used to go through all the existing Mutation and Query
+    // in a page and fetch the necessary data before rendering
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { apollo, Component, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
 MyApp.propTypes = {
-  Component: PropTypes.func.isRequired
+  apollo: PropTypes.object.isRequired,
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.object
 };
 
-export default MyApp;
+MyApp.defaultProps = {
+  pageProps: {}
+};
+
+export default withData(MyApp);
